ca99082035d32d08343de00b352729efbcb5cdf9
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-06-25 18:30:41 +1000

    update some classes in the html and add psuedo code to the JS file

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+// get search input to save to local storage and become a list
+// use input to retive information from open weather
+// once information is retived display it in the current city container
+// retive the 5 day forecast 
+// display the 5 day for5cast in the 5 day forecast cards  
